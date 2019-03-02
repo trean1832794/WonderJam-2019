15d77bd990c111408d5447ad0bd101c42fac7ae2
Guillaume <Guillaumelm@msn.com>
2019-03-02 00:51:47 -0500

    First stash unstashed

---
+++ Assets/PowerUps/jumpUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpUp : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Activate(GameObject playerConcerned) {
+
+        playerConcerned.GetComponent<DefaultMovement>().baseNbJumps++;
+
+    }
+
+    public void Deactivate(GameObject playerConcerned) {
+
+        playerConcerned.GetComponent<DefaultMovement>().baseNbJumps++;
+
+    }
+}
---
+++ Assets/PowerUps/testPowerup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPowerup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate (GameObject playerConcerned)
+    {
+
+        Debug.Log("Activé! par " + playerConcerned.name);
+
+    } 
+
+    public void Deactivate (GameObject playerConcerned)
+    {
+
+        Debug.Log("Désactivé! par " + playerConcerned.name);
+
+    }
+}
