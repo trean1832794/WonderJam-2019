72ca592cb599ff8ff6906fd311e25b681bbe870d
Mattys Gervais <mattysgervais@gmail.com>
2019-03-02 15:16:07 -0500

    Menu Upgrades
    Options work, death screen

---
+++ Assets/Menu/StatGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject faded;
+    void Start()
+    {
+        faded=GameObject.Find("Faded");
+    }
+    public void callCamera() {
+        faded.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
