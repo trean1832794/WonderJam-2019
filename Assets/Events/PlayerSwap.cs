75fb3ebe5baff27f01f6cbeae4f8938ffb3a81e2
Thomas Bergeron <Craftman3512@gmail.com>
2019-03-02 14:45:16 -0500

    Beautiful graphics and Events!

---
+++ Assets/Events/PlayerSwap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwap : MonoBehaviour
+{
+
+    private GameObject player1;
+    private GameObject player2;
+    private Vector3 player1Pos;
+    private Vector3 player2Pos;
+
+    public void Activate ()
+    {
+
+        //do stuff
+        player1 = GameObject.Find("Player1");
+        player2 = GameObject.Find("Player2");
+        player1Pos = player1.transform.position;
+        player2Pos = player2.transform.position;
+
+        player1.transform.position = player2Pos;
+        player2.transform.position = player1Pos;
+
+    }
+
+}
---
+++ Assets/Events/Teleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : StateMachineBehaviour
+{
+
+    private bool teleported = false;
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        if (stateInfo.normalizedTime >= 0.1714f && !teleported)
+        {
+
+            //teleport players
+            GameObject.Find("Event").GetComponent<PlayerSwap>().Activate();
+            teleported = true;
+
+        }
+
+        if (stateInfo.normalizedTime >= 0.6f)
+        {
+
+            Destroy(animator.gameObject);
+
+        }
+
+    }
+
+}
