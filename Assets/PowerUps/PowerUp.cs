cdef7202a1fda3915d35ec094b33ebf688a313d7
Guillaume <Guillaumelm@msn.com>
2019-03-02 17:02:31 -0500

    Power up update for bug test

--- Assets/PowerUps/PowerUp.cs
+++ Assets/PowerUps/PowerUp.cs
@@ -25,13 +25,6 @@ public class PowerUp : MonoBehaviour
 
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-
-
-    }
 
     // Update is called once per frame
     void Update()
