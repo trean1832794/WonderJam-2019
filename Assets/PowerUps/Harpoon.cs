22d463d278ca2afc7ab16992d1ac14a1623d0138
Thomas Bergeron <Craftman3512@gmail.com>
2019-03-02 19:35:45 -0500

    Finished Harpoon Powerup and did a little cleaning on the files

--- Assets/PowerUps/Harpoon.cs
+++ Assets/PowerUps/Harpoon.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Harpoon : MonoBehaviour {
 
+    public GameObject harpoon;
+
     public void Activate(GameObject playerConcerned) {
 
-        //Stun for when we'll create the harpoon.
-        //GameObject.Find("Player" + ((playerConcerned.GetComponent<DefaultMovement>().player % 2) + 1)).GetComponent<DefaultMovement>().canMove = false;
+       GameObject newHarpoon = Instantiate(harpoon, transform.position,Quaternion.identity);
+        newHarpoon.GetComponent<HarpoonObject>().targetPos = GameObject.Find("Player" + ((playerConcerned.GetComponent<DefaultMovement>().player % 2) + 1)).transform.position;
+        newHarpoon.GetComponent<HarpoonObject>().target = GameObject.Find("Player" + ((playerConcerned.GetComponent<DefaultMovement>().player % 2) + 1));
 
     }
 
     public void Deactivate(GameObject playerConcerned) {
 
-        //De-stuns
-        //GameObject.Find("Player" + ((playerConcerned.GetComponent<DefaultMovement>().player % 2) + 1)).GetComponent<DefaultMovement>().canMove = true;
+
 
     }
 
