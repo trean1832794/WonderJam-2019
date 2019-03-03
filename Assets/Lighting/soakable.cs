b5b3b91e9b085318e9c703fd302ec92b940804df
Guillaume <Guillaumelm@msn.com>
2019-03-03 12:40:52 -0500

    Added "soakable" script to torches. Fixed winning player's label.
    Also added torches to main Game scene

--- Assets/Lighting/soakable.cs
+++ Assets/Lighting/soakable.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class soakable : MonoBehaviour {
+    bool soak = false;
+
+    private void Update() {
+        if (soak == true) {
+            GetComponentInChildren<Light>().intensity -= 0.70f;
+            if (GetComponentInChildren<Light>().intensity <= 0) {
+                GetComponentInChildren<Light>().enabled = false;
+            }
+        }
+    }
+    
     // Start is called before the first frame update
     public void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag.Equals("Water")) {
-            GetComponent<Light>().enabled = false;
+            this.GetComponent<Animator>().SetTrigger("Burn");
+            GetComponentInChildren<lightPulse>().enabled = false;
+            soak = true;
         }
     }
 }
