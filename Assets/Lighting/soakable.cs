0233f7d6a60dd04e320aebcce0a50c6ade66550a
Guillaume <Guillaumelm@msn.com>
2019-03-03 11:38:14 -0500

    Added water tag and torch scripts

---
+++ Assets/Lighting/soakable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soakable : MonoBehaviour {
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag.Equals("Water")) {
+            GetComponent<Light>().enabled = false;
+        }
+    }
+}
