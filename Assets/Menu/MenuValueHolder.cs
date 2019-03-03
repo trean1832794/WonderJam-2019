6b9248f7ef3f0251c46334582d86f2d48b73b68c
Mattys Gervais <mattysgervais@gmail.com>
2019-03-03 09:23:10 -0500

    MenuFixes+Upgrades
    MainMenu in deathscreen now works, faded appears too.

--- Assets/Menu/MenuValueHolder.cs
+++ Assets/Menu/MenuValueHolder.cs
@@ -11,17 +11,28 @@ public class MenuValueHolder : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         difficulty = 1;
+        Color cyan = new Color32(24, 95, 100, 255);
         togg.onValueChanged.AddListener(delegate {
             isGhostPlatforms = togg.isOn;
         });
         easy.onClick.AddListener(delegate {
             difficulty = 1;
+            easy.GetComponent<Text>().color= new Color32(0, 200, 0, 255);
+            med.GetComponent<Text>().color = cyan;
+            hard.GetComponent<Text>().color = cyan;
+
         });
         med.onClick.AddListener(delegate {
             difficulty = 2;
+            easy.GetComponent<Text>().color = cyan;
+            med.GetComponent<Text>().color = new Color32(225, 125, 0, 255);
+            hard.GetComponent<Text>().color = cyan;
         });
         hard.onClick.AddListener(delegate {
             difficulty = 3;
+            easy.GetComponent<Text>().color = cyan;
+            med.GetComponent<Text>().color = cyan;
+            hard.GetComponent<Text>().color = new Color32(200, 0, 0, 255);
         });
     }
 
